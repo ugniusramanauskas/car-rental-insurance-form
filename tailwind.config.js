b286b45273b3af4b9a3f50d41af35fd8dd906a17
Ugnius Ramanauskas <ugnius.ramanauskas@gmail.com>
2025-07-09 21:05:25 +0300

    Add custom font and font size configurations to Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      fontSize: {
+        'xl': ['20px', '24px'], // [font-size, line-height]
+        'label': ['16px', '24px'], // Custom size for form labels
+      },
+    },
   },
   plugins: [],
 };
