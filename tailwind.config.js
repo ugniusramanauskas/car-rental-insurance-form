854dc6ddefecd7418c8e369153cb85c601aaec28
Ugnius Ramanauskas <ugnius.ramanauskas@gmail.com>
2025-07-12 11:43:30 +0300

    Update HTML structure and enhance Tailwind CSS configuration for improved styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,16 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+        open: ['"Open Sans"', 'sans-serif'],
+        helvetica: ['"Helvetica Neue"', 'sans-serif'],
       },
       fontSize: {
         'xl': ['20px', '24px'], // [font-size, line-height]
         'label': ['16px', '24px'], // Custom size for form labels
+      },
+      boxShadow: {
+        'custom-glow': '0 0 2px rgba(87, 87, 87, 0.83)',
+        'glow-soft': '0 0 2.63px rgba(87, 87, 87, 0.37)',
       },
     },
   },
