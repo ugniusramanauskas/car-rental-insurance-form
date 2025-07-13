1419dd53e5491a9e9e9ddf68cfd2611e92d4d232
Ugnius Ramanauskas <ugnius.ramanauskas@gmail.com>
2025-07-13 17:40:16 +0300

    Update AppHeader background color for improved visual consistency

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="bg-gray-800 text-white text-center py-[19px]">
+  <header class="bg-[#2E2E2E] text-white text-center py-[19px]">
     <h1 class="text-2xl font-bold flex items-center justify-center">
       <img src="../assets/logo.svg" alt="Logo" />
     </h1>
